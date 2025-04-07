36fe724565b62ebf1a8fa89aa80cde406c1bee8d
maridabrins <maridabrins@hotmail.com>
2025-04-07 15:52:01 -0300

    feat: adicionando metodo POST a classe UsuarioController

--- src/main/java/com/revisao/ecommerce/controllers/UsuarioController.java
+++ src/main/java/com/revisao/ecommerce/controllers/UsuarioController.java
@@ -2,8 +2,15 @@ package com.revisao.ecommerce.controllers;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revisao.ecommerce.dto.UsuarioDTO;
+import com.revisao.ecommerce.services.UsuarioService;
 
 
 
@@ -11,26 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "usuario")
 public class UsuarioController {
-	/*
-	@Autowired
-	UsuarioService service;
-	*/
-	/*@Autowired
-	UsuarioRepository repository;
 	
-	@GetMapping
-	public ResponseEntity<List<Usuario>> mostrarTodos(){
-		List<Usuario> user = repository.findAll();
-		return ResponseEntity.ok(user);
-	}
-	
-	@GetMapping
-	public ResponseEntity<?> mostrarPorId(@PathVariable Long id){
-		Usuario user = repository.getById(id);
-		return ResponseEntity.ok(user);
+		@Autowired
+		UsuarioService usuarioService; //instanciando service para acessarmos o metodo
 		
-	}
-*/
+		@PostMapping(value = "/cadastrar")
+		public ResponseEntity <UsuarioDTO> salvarUsuario(@RequestBody UsuarioDTO dto){ //salvando usuarios
+			dto = usuarioService.salvarUsuario(dto);
+			return ResponseEntity.ok(dto);
+		}
 	
 	
 }
