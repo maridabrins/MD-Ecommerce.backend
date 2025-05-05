e4ad7c3cf0b011dded4775141a656a55f49996cf
maridabrins <maridabrins@hotmail.com>
2025-05-05 15:59:11 -0300

    feat: adicionando construtores, getters e setters no ItemDoPedidoDTO

--- src/main/java/com/revisao/ecommerce/dto/ItemDoPedidoDTO.java
+++ src/main/java/com/revisao/ecommerce/dto/ItemDoPedidoDTO.java
@@ -5,25 +5,31 @@ import com.revisao.ecommerce.entities.ItemDoPedido;
 public class ItemDoPedidoDTO {
 	
 	private Long produtoId;
-	private Integer quantidade;
+	private String nome;
+	private int quantidade;
 	private Double preco;
+	private String urlImg;
 	
 	
 	public ItemDoPedidoDTO() {
 		
 	}
-	
-	
-	public ItemDoPedidoDTO(Long produtoId, Integer quantidade, Double preco) {
-	    this.produtoId = produtoId;
-	    this.quantidade = quantidade;
-	    this.preco = preco;
+
+
+	public ItemDoPedidoDTO(Long produtoId, String nome, int quantidade, Double preco, String urlImg) {
+		this.produtoId = produtoId;
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+		this.urlImg = urlImg;
 	}
 	
 	public ItemDoPedidoDTO(ItemDoPedido entity) {
-	    this.produtoId = entity.getProduto().getId(); 
-	    this.quantidade = entity.getQuantidade();
-	    this.preco = entity.getPreco();
+		this.produtoId = entity.getProduto().getId(); //pegando o id do Produto
+		this.nome = entity.getProduto().getNome(); //pegando o nome do Produto
+		this.quantidade = entity.getQuantidade();
+		this.preco = entity.getPreco();
+		this.urlImg = entity.getProduto().getImgUrl(); //pegando url do Produto
 	}
 
 
@@ -37,12 +43,22 @@ public class ItemDoPedidoDTO {
 	}
 
 
-	public Integer getQuantidade() {
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getQuantidade() {
 		return quantidade;
 	}
 
 
-	public void setQuantidade(Integer quantidade) {
+	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
 	}
 
@@ -57,6 +73,18 @@ public class ItemDoPedidoDTO {
 	}
 
 
-	
+	public String getUrlImg() {
+		return urlImg;
+	}
 
+
+	public void setUrlImg(String urlImg) {
+		this.urlImg = urlImg;
+	}
+	
+	public Double getSubTotal() {
+		return preco * quantidade;
+	}
+	
+	
 }
