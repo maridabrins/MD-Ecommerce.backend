fc38b3dbb6cae7eac06f776e7719cdc33e6267c9
maridabrins <maridabrins@hotmail.com>
2025-05-05 16:02:45 -0300

    feat: adicionando anottation do repository

--- src/main/java/com/revisao/ecommerce/repositories/PedidoRepository.java
+++ src/main/java/com/revisao/ecommerce/repositories/PedidoRepository.java
@@ -1,9 +1,12 @@
 package com.revisao.ecommerce.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revisao.ecommerce.entities.Pedido;
-
+@Repository
 public interface PedidoRepository extends JpaRepository<Pedido,Long>{
+	
 
 }
